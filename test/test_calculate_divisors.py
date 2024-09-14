a2c3089ac456a865be51572e6e185f9ac67f20f1
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-14 18:10:47 +0100

    add clarity to the test descriptions for calculate_divisors()

--- test/test_calculate_divisors.py
+++ test/test_calculate_divisors.py
@@ -10,6 +10,6 @@ def test_returns_3_given_one_multiple_of_3():
 def test_returns_8_given_one_multiple_of_3_and_one_of_5():
     assert calculate_divisors(6) == 8
 
-def test_returns_correct_total_given_many_multiples():
+def test_calculates_total_given_many_multiples_of_3_and_5():
     assert calculate_divisors(10) == 23
     assert calculate_divisors(12) == 33
